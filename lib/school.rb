3fb20757bb39b4f33fe4106b04cf8f6ee5c1d80d
Nicholas Dover <dovern42@gmail.com>
2019-02-20 21:09:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School (name)
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade].push(name)
+    else
+      @roster[grade] = [name]
+    end
+  end
+  def grade(num)
+    if @roster.has_key?(num)
+      @roster[num]
+    end
+  end
+  def sort
+    newroster ={}
+    @roster.each {|grade, students| ([grade] => students.sort) => newroster}
+    @roster = newroster
+  end
+end
